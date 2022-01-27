65fa1d98daecf9fe839ea76a552d7b53fc0093cb
Esther Kang <eskang87@gmail.com>
2022-01-26 17:46:58 -0800

    commit tree 4c280835516d726b7f692ae4a8a68f21446cc2b4

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+class Polygon {
+  constructor(sides){
+    this.sides = sides
+  };
+  get countSides(){
+    return this.sides.length
+  };
+  get perimeter(){
+    return this.sides.reduce((pV, cV) => pV + cV)
+  };
+};
+
+class Triangle extends Polygon {
+  get isValid(){
+    return (((this.sides[0] + this.sides[1]) > this.sides[2]) && ((this.sides[0] + this.sides[2]) > this.sides[1]) && ((this.sides[1] + this.sides[2]) > this.sides[0]))
+  };
+};
+
+class Square extends Polygon {
+  get isValid(){
+    if(this.countSides !== 4) return
+    return this.sides[0] === this.sides[1] && this.sides[0] === this.sides[2] && this.sides[0] === this.sides[3]
+  };
+  get area(){
+    return this.sides[0] * this.sides[0]
+  }; 
+};
